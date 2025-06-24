2f1e5eca12758d50b7edfb3f52f557cab31afe3a
Sam Wells <samwells95@gmail.com>
2025-06-23 23:23:13 -0700

    Update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.gradle.kotlin.dsl.testImplementation
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.flywaydb.flyway") version "11.9.1"
 }
 
 group = "io.samwells"
@@ -11,6 +14,15 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
+}
+
+flyway {
+    url = System.getenv("SPRING_DATASOURCE_URL") ?: "jdbc:postgresql://localhost:5432/user_service"
+    user = System.getenv("SPRING_DATASOURCE_USERNAME") ?: "postgres"
+    password = System.getenv("SPRING_DATASOURCE_PASSWORD") ?: "postgres"
+    locations = arrayOf("classpath:db/migration")
+    baselineOnMigrate = true
+    validateOnMigrate = true
 }
 
 configurations {
@@ -28,10 +40,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.flywaydb:flyway-database-postgresql")
+	implementation("org.postgresql:postgresql:42.7.7")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
